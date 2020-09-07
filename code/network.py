d165520a86a62b4e5ab2490b8165119cd5aac1ee
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-07 13:09:50 +0800

    debug

--- code/network.py
+++ code/network.py
@@ -300,10 +300,10 @@ class SCDeepCluster(object):
                 index = 0
             else:
                 print(index, batch_size)
-                print("x_counts : ", x_counts.shape, x_counts.dtype)
-                print("sf : ", sf.shape, sf.dtype)
-                print("p : ", p.shape, p.dtype)
-                print("raw_counts : ", raw_counts.shape, raw_counts.dtype)
+                print("x_counts : ", type(x_counts), x_counts.shape, x_counts.dtype)
+                print("sf : ", type(sf), sf.shape, sf.dtype)
+                print("p : ", type(p), p.shape, p.dtype)
+                print("raw_counts : ", type(raw_counts), raw_counts.shape, raw_counts.dtype)
                 # print(x_counts.shape, sf.shape, p.shape, row_counts.shape)
                 loss = self.model.train_on_batch(x=[x_counts[index * batch_size:(index + 1) * batch_size], 
                                                     sf[index * batch_size:(index + 1) * batch_size]],
